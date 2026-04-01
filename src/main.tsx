f70ca6a694c3c674a92ad4844ace4832f577b14c
zmoss4 <zachmoss04@gmail.com>
2026-04-01 13:23:41 -0500

    [Blink] Fix "useBlink must be used within a BlinkProvider" error • Added BlinkProvider to src/main.tsx • Updated BlinkAuthProvider usage to be nested within BlinkProvider • Used environment variables for projectId and publishableKey in BlinkProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui';
-import { BlinkAuthProvider } from '@blinkdotnew/react';
-import { blink } from './blink/client';
+import { BlinkProvider, BlinkAuthProvider } from '@blinkdotnew/react';
 import App from './App';
 import './index.css';
 
@@ -16,14 +15,19 @@ const queryClient = new QueryClient({
   },
 });
 
+const projectId = import.meta.env.VITE_BLINK_PROJECT_ID;
+const publishableKey = import.meta.env.VITE_BLINK_PUBLISHABLE_KEY;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BlinkUIProvider theme="midnight" darkMode="system">
-        <BlinkAuthProvider client={blink}>
-          <Toaster position="top-right" />
-          <App />
-        </BlinkAuthProvider>
+        <BlinkProvider projectId={projectId} publishableKey={publishableKey}>
+          <BlinkAuthProvider>
+            <Toaster position="top-right" />
+            <App />
+          </BlinkAuthProvider>
+        </BlinkProvider>
       </BlinkUIProvider>
     </QueryClientProvider>
   </React.StrictMode>,
